cddfc211ee0f31c0c88e21343da1dcaa6929916f
Chloe J <chloexxyzz@gmail.com>
2021-04-30 17:55:23 -0400

    Game now runs fine, starting state is the R-pentomino

--- src/GameOfLife.pl
+++ src/GameOfLife.pl
@@ -16,13 +16,15 @@ use Automata::Cells;
 
 
 my %starting_cells = (
-  "20,20"=>"1",
-  "20,21"=>"1",
-  "20,22"=>"1"
+  "20,27"=>"1",
+  "20,28"=>"1",
+  "20,29"=>"1",
+  "21,28"=>"1",
+  "19,29"=>"1"
 );
 my $ref_cells = \%starting_cells;
 my $cells = new Automata::Cells($ref_cells);
-my $board = new Board::TerminalBoard(50,50,0,new Curses, $cells);
+my $board = new Board::TerminalBoard(40,55,0,new Curses, $cells);
 # Use string description of coordinates, perl does not support array as key
 
 do {
--- src/lib/Automata/Cells.pm
+++ src/lib/Automata/Cells.pm
@@ -20,6 +20,51 @@ sub hasAlive {
 
 sub applyRules {
   my ($self) = @_;
+  my %new_cells = ();
+  my %dead_cells = ();
+  my $href = $self->{all};
+  # rule 1+3 (process live cells): any cell with 2 or 3 live 
+  #                                neighbors remains alive
+  # rule 2: any dead cell with 3 living neighbors becomes alive
+  for my $alive_cell (keys %$href) {
+    my ($x_str, $y_str) = split /,/, $alive_cell;
+    my $x = int($x_str);
+    my $y = int($y_str); 
+    my $count_alive = 0;
+    for (my $i = ($x - 1); $i <= ($x + 1); $i++) {
+      for (my $j = ($y - 1); $j <= ($y + 1); $j++) {
+        if (exists $self->{all}->{$i.",".$j}) {
+          if (($i != $x) || ($j != $y)) {
+            $count_alive++;
+          }
+          next;
+        }
+        my $count_dead = 0;
+        my @potential_neighbors = 
+            (($i-1).",".($j-1),($i-1).",".($j),($i-1).",".($j+1),($i).",".($j-1),
+            ($i).",".($j+1),($i+1).",".($j-1),($i+1).",".($j),($i+1).",".($j+1));
+        foreach my $potential (@potential_neighbors) {
+          if (exists $self->{all}->{$potential}) {
+            $count_dead++;
+          }
+        }
+        if ($count_dead == 3) {
+          $new_cells{$i.",".$j} = "1";
+        }
+      }
+    }
+    if (($count_alive != 2) && ($count_alive != 3)) {
+      $dead_cells{$x.",".$y} = "1";
+    }
+  }  
+  # add the new cells,     
+  for my $new_cell (keys %new_cells) {
+    $self->{all}->{$new_cell} = "1";
+  }
+  # remove the dead cells
+  for my $dead_cell (keys %dead_cells) {
+    delete $self->{all}->{$dead_cell};
+  }
   return 1;
 }
 
